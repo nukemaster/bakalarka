1224c789df69d067b0a9ac2bd04b5ccc4421bfce
Petr <dymacek.p@seznam.cz>
2016-10-20 11:19:59 +0200

    Funkční detekce

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    /*
+
     Detection detection;
     DetectionThread detectionThread(&detection);
     detectionThread.start();
     //detection.show(); // todo: dodelat posuvniky pro nastaveni detekce
     QObject::connect(&detection, SIGNAL(newCoordinates(int,int)), &w, SLOT(coordinatesFromDetection(int,int)) );
-    */
+
 
     return a.exec();
 }
